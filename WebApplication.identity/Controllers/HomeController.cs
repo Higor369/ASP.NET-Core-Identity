9a9d403fb9575b1a1526b6b41332713286d0433a
Higor <higorpereirabarbosa@gmail.com>
2021-03-16 23:00:18 -0300

    primeiro commit, estrutura de cookies criada , primeira parte do curso

---
+++ WebApplication.identity/Controllers/HomeController.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using WebApplication.identity.Models;
+
+namespace WebApplication.identity.Controllers
+{
+
+    //[ApiController} => valida automaticamente todos data anotations
+    public class HomeController : Controller
+    {
+        private UserManager<MyUser> _UserManager;
+
+        public HomeController(UserManager<MyUser> userManager)
+        {
+            _UserManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = await _UserManager.FindByNameAsync(model.UserName);
+
+                if( user == null)
+                {
+                    user = new MyUser()
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        UserName = model.UserName
+
+                    };
+
+                    var result = await _UserManager.CreateAsync(user, model.Password);
+                }
+
+                return View("Sucess");
+            }
+
+            return View();
+
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _UserManager.FindByNameAsync(model.UserName);
+
+                if(user != null && await _UserManager.CheckPasswordAsync(user, model.Password))
+                {
+                    var identity = new ClaimsIdentity("cookies");
+                    identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.Id));
+                    identity.AddClaim(new Claim(ClaimTypes.Name, user.UserName));
+
+                    await HttpContext.SignInAsync("coockies", new ClaimsPrincipal(identity));
+
+                    return RedirectToAction("About");
+                }
+
+
+                ModelState.AddModelError("", "Usuario ou senha invalida" );
+            }
+
+            return View();
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            return View();
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Sucess()
+        {
+            return View();
+        }
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication.identity/MyUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.identity
+{
+    public class MyUser
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string NormalizedUsername { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
